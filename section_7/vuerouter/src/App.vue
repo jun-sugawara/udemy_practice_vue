11179abd3c16842c1bd97e4e3b33203b5811d757
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-09-22 19:54:11 +0900

    動的パラメーター

--- section_7/vuerouter/src/App.vue
+++ section_7/vuerouter/src/App.vue
@@ -3,8 +3,8 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <!-- タグをボタンにしただけなので一旦コメントアウト<router-link to="/about" tag="button">About</router-link> -->
-      <router-link to="/about" exact-active-class="test">About</router-link>
-      <router-link to="/book">BookList</router-link>
+      <router-link to="/about" exact-active-class="test">About</router-link> |
+      <router-link to="/book">BookList</router-link> |
     </nav>
     <router-view/>
   </div>
--- section_7/vuerouter/src/components/BookDetail.vue
+++ section_7/vuerouter/src/components/BookDetail.vue
@@ -3,6 +3,7 @@
     <p>本の詳細</p>
     <p>タイトル： {{ title }}</p>
     <p>本の内容： {{ content }}</p>
+    {{ books[bookIndex].title }}
   </div>
 </template>
 
@@ -12,6 +13,21 @@ export default {
     id: Number,
     title: String,
     content: String
+  },
+  data(){
+    return {
+      bookIndex: this.$route.params.id -1,
+      books:[
+        {id: 1, title:'タイトル１', content:'本の内容１'},
+        {id: 2, title:'タイトル２', content:'本の内容２'},
+        {id: 3, title:'タイトル３', content:'本の内容３'}
+      ]
+    }
+  },
+  created(){
+    if(this.$route.params.id > this.books.length){
+      this.$router.push(('/book'))
+    }
   }
 }
 </script>
