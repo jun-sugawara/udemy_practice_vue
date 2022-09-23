97bb3359d5bce24d28be9665ed75824fe2de9331
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-09-23 09:13:55 +0900

    finish section_7

--- section_7/vuerouter/src/views/HomeView.vue
+++ section_7/vuerouter/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="checkProcessEnv">processEnvを表示</button>
   </div>
 </template>
 
@@ -13,6 +14,10 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },methods:{
+    checkProcessEnv(){
+      console.log(process.env)
+    }
   }
 }
 </script>
