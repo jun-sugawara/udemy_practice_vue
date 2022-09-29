0b8f084781493cae18d42c1dc28dd55cee2da87f
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-09-30 07:29:59 +0900

    editページの内容をローカルストレージに保存して、保存後はトップへ遷移

--- section_8/bookapp/src/pages/BookEdit.vue
+++ section_8/bookapp/src/pages/BookEdit.vue
@@ -24,7 +24,7 @@
               感想：<v-textarea class="mx-2" v-model="book.memo">{{ book.memo }}</v-textarea>
               <v-card-actions>
                 <v-btn color="secondary" to="/">一覧に戻る</v-btn>
-                <v-btn color="info" >保存する</v-btn>
+                <v-btn color="info" @click="updateBookInfo">保存する</v-btn>
               </v-card-actions>
             </v-col>
           </v-row>
@@ -45,6 +45,15 @@ export default {
       book: '',
       date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       modal: false
+    }
+  },
+  methods:{
+    updateBookInfo(){
+      this.$emit('update-book-info',{
+        id: this.$route.params.id,
+        readDate: this.date,
+        memo: this.book.memo
+      })
     }
   },
   beforeRouteEnter(to, from, next){
