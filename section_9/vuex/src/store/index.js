2dcf39c04ed7ce3495e1061f70447599297a1712
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-10-08 22:27:43 +0900

    VueStateとmutationsを使う

--- section_9/vuex/src/store/index.js
+++ section_9/vuex/src/store/index.js
@@ -4,13 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
+  state: { // 初期値
+    count: 0
   },
   mutations: {
+    increment( state ){
+      state.count++
+    }
   },
   actions: {
+  },
+  getters: {
   },
   modules: {
   }
