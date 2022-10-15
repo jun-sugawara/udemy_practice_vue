a70a2586dbdf3b5a020418aaec9fb5d6e6687b92
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-10-15 11:05:24 +0900

    CompositionAPIを使った実装の準備。画面描画

---
+++ section_10/vue3-test/src/views/CompositionTest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Composition
+  </div>
+</template>
+
+<script>
+export default {
+  data(){},
+  setup(){
+    console.log('setup')
+  },
+  created(){
+    console.log('created')
+  },
+  mounted(){
+    console.log('mounted')
+  }
+}
+</script>
+
+<style>
+
+</style>
