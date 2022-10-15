d238449518c1158b1197a327cea5130043824c37
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-10-15 19:38:16 +0900

    toRefsメソッドの利用

--- section_10/vue3-test/src/views/CompositionTest.vue
+++ section_10/vue3-test/src/views/CompositionTest.vue
@@ -6,11 +6,13 @@
     <p>ref: {{ nameRef }}</p>
     <p>reactive: {{ book.title }}</p>
     <p>reactive: {{ book.author[0] }}</p>
+    <p>reactiveToRefs: {{ titleRef }}</p>
+    <p>reactiveToRefs: {{ authorRef[0] }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue'
+import { reactive, ref, toRefs } from 'vue'
 export default {
   setup(){
     let name = '八村'
@@ -20,11 +22,16 @@ export default {
       title: 'タイトル',
       author: ['大谷','伊藤']
     })
+    const bookToRefs = reactive({
+      titleRef: 'タイトル2',
+      authorRef: ['大谷2','伊藤2']
+    })
     return {
       name,
       age,
       nameRef,
-      book
+      book,
+      ...toRefs(bookToRefs)
     }
   },
   data(){
