9fef4380501d02107fae01e27b3aece21229da46
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-10-22 11:41:28 +0900

    scriptSetupを使ってみる

---
+++ section_11/script_setup_test/src/components/ScriptSetupTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+</template>
